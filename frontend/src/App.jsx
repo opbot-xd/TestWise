e4884fbb31ce5868f8daa39f79cc3cbad9f983b9
eclipsedd <aryan05ch@gmail.com>
2024-03-30 16:55:39 +0530

    test1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css'
-import './Components/LoginSignup/LoginSignup.jsx'
-import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
-import HomeList from "./Components/HomeList.tsx";
-
 function App() {
   return (
     <>
-      <LoginSignup />
+      
     </>
 
   );
